c388931fdad6b366a53fc8fa1b478883ada1354a
Henryyugsi <henryyugsin@gmail.com>
2023-05-25 17:46:18 -0500

    Terceroi

--- principal.js
+++ principal.js
@@ -12,7 +12,9 @@ var io = require('socket.io')(http)
 app.get("/prueba.css",(req,res)=>{
     res.status(200).sendFile(path.resolve(__dirname,'./prueba.css'));
 });
-
+app.get("/css/nosotros.css",(req,res)=>{
+    res.status(200).sendFile(path.resolve(__dirname,'./css/nosotros.css'));
+});
 //Enviamos las imagenes
 app.get("/images/logo.jpg",(req,res)=>{
     res.status(200).sendFile(path.resolve(__dirname,'./images/logo.jpg'));
@@ -23,14 +25,44 @@ app.get("/images/promocion1.jpg",(req,res)=>{
 app.get("/images/promocion2.jpg",(req,res)=>{
     res.status(200).sendFile(path.resolve(__dirname,'./images/promocion2.jpg'));
 });
+app.get("/images/promocion3.jpg",(req,res)=>{
+    res.status(200).sendFile(path.resolve(__dirname,'./images/promocion3.jpg'));
+});
+app.get("/images/nosotros.jpg",(req,res)=>{
+    res.status(200).sendFile(path.resolve(__dirname,'./images/nosotros.jpg'));
+});
 app.get("/images/redessociales.jpg",(req,res)=>{
     res.status(200).sendFile(path.resolve(__dirname,'./images/redessociales.jpg'));
 });
-//Enviamos la pagina principal
+app.get("/images/logo-pie-pagina.jpg",(req,res)=>{
+    res.status(200).sendFile(path.resolve(__dirname,'./images/logo-pie-pagina.jpg'));
+});
+app.get("/images/promocion1-telefono.jpg",(req,res)=>{
+    res.status(200).sendFile(path.resolve(__dirname,'./images/promocion1-telefono.jpg'));
+});
+app.get("/images/promocion2-telefono.jpg",(req,res)=>{
+    res.status(200).sendFile(path.resolve(__dirname,'./images/promocion2-telefono.jpg'));
+});
+app.get("/images/promocion3-telefono.jpg",(req,res)=>{
+    res.status(200).sendFile(path.resolve(__dirname,'./images/promocion3-telefono.jpg'));
+});
+//Enviamos las paginas
 app.get("/",(req,res)=>{
     res.status(200).sendFile(path.resolve(__dirname,'prueba.html'));
 });
-
+app.get('/speedytest', (req,res)=>{
+    res.status(200).sendFile(path.resolve(__dirname,'./html/speedytest.html'));
+});
+app.get('/nosotros', (req,res)=>{
+    res.status(200).sendFile(path.resolve(__dirname,'./html/nosotros.html'));
+});
+app.get('/contacto', (req,res)=>{
+    res.status(200).sendFile(path.resolve(__dirname,'./html/contacto.html'));
+});
+app.get('/puntosdepago', (req,res)=>{
+    res.status(200).sendFile(path.resolve(__dirname,'./html/puntosdepago.html'));
+});
+//Iniciamos el servidor
 var server = http.listen(8081,()=>{
     console.log("Servidor corriendo en el puerto:",
     server.address().port);
